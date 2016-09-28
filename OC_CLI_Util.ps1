83d4088d82c93456f7b60509e402e46c605a4c51
mmasko <mymas81@gmail.com>
2016-09-28 07:17:43 -0700

    Updates to .ps1

--- OC_CLI_Util.ps1
+++ OC_CLI_Util.ps1
@@ -2,7 +2,7 @@
 #State of California, Department of Technology Services, Office of Digital Innovation
 
 #Replace $null with the URL of your OpenShift Environment withouth the forward slash. ex. "https://openshift.com"
-$url = $null
+$url = "https://ose.csil.cdt.ca.gov:8443"
 
 #Fill out username and password and remove comments to keep your credentials permanently in place.
 #This prevents having to enter your credentials every time, but reduces security.
@@ -32,9 +32,11 @@ cls
 "1: Login to OpenShift"
 "2: Select Project"
 "3: Select Pod"
-"4: Create New Project"
+"4: Select Latest Build"
+"5: Create New Project"
+"6: Edit Build Config"
 "c: Custom Command"
-#"i: Configure OC First Time" Coming Soon
+"i: Configure OC First Time"
 "r: Rsync to active pod"
 "l: Logout of OpenShift"
 "q: Quit without logout"
@@ -54,52 +56,68 @@ Clear-Variable -name password -Scope script
 
 function showenvironment {
 "=======Your Environment========="
-#"OSE: $ose"
 "Project: $env"
 "Pod: $workpod"
+"Build: $latestbuild"
 "remote: $remotefolder"
 "Local: $localfolder"
 "================================"
 }
 
+
 function getprojects { 
                        
-                        $projects = oc get projects
-                        $output = $projects[1..$projects.Length]
-                        $i = 1
-                        $menuitem = @()
-                        foreach ($_ in $output -match "Active") {
-                        "$i $_"
-                        $Array = $_.Split(" ")[0]
-                        $menuitem += $Array
-                        $i++
-                        }
-                        $MenuSel = Read-Host -prompt "Select which project to use"
-                        $script:env = $menuitem[$MenuSel-1]
-                        oc project $menuitem[$MenuSel-1]
+$projects = oc get projects
+$output = $projects[1..$projects.Length]
+$i = 1
+$menuitem = @()
+    foreach ($_ in $output -match "Active") {
+    "$i $_"
+    $Array = $_.Split(" ")[0]
+    $menuitem += $Array
+    $i++
+    }
+$MenuSel = Read-Host -prompt "Select which project to use"
+$script:env = $menuitem[$MenuSel-1]
+oc project $menuitem[$MenuSel-1]
 }
 
 function getpods {
-                        $pods = oc get pods
-                        $pod = $pods[1..$pods.Length]
-                        $i = 1
-                        $podmenu = @()
-                        foreach ($_ in $pod -match "running") {
-                        "$i $_"
-                        $podArray = $_.Split(" ")[0] 
-                        $podmenu += $podArray
-                        $i++
-                        }
-                        $podMenuSel = Read-Host -prompt "Select which pod to use"
-                        $script:workpod = $podmenu[$podMenuSel-1]
-                        $script:remotefolder = oc rsh "$workpod" "pwd"
+$pods = oc get pods
+$pod = $pods[1..$pods.Length]
+$i = 1
+$podmenu = @()
+    foreach ($_ in $pod -match "running") {
+    "$i $_"
+    $podArray = $_.Split(" ")[0] 
+    $podmenu += $podArray
+    $i++
+    }
+$podMenuSel = Read-Host -prompt "Select which pod to use"
+$script:workpod = $podmenu[$podMenuSel-1]
+$script:remotefolder = oc rsh "$workpod" "pwd"
+}
+
+function getlatestbuild {
+$services = oc get service
+$service = $services[1..$pods.Length]
+$i = 1    
+$servicemenu=@()
+    foreach ($_ in $services) {
+    "$i $_"
+    $servicearray = $_.Split(" ")[0]
+    $servicemenu += $servicearray
+    $i++
+    }
+$servicemenusel = Read-Host -Prompt "Select your active service"
+$script:latestbuild = $servicemenu[$servicemenusel-1]
+return
 }
 
 function rsync {
 $b = ":"
 $o = "oc rsync --no-perms=true $localfolder $workpod$b$remotefolder"
 Invoke-Expression $o
-return
 pause
 }
 
@@ -109,7 +127,6 @@ $command = Read-Host "Type custom command here"
 Invoke-Expression $command
 $command
 pause
-
 }
 
 function OSlogin {
@@ -118,7 +135,9 @@ requesttoken
 invoke-expression $login
 getprojects
 getpods
-
+getlatestbuild
+cls
+return
 }
 
 function requesttoken {
@@ -128,7 +147,7 @@ $ie = New-Object -com InternetExplorer.Application
 $ie.visible=$false
 $ie.navigate("$oauthurl") 
 while($ie.ReadyState -ne 4) {start-sleep -m 100} 
-$ie.document.getElementById("inputUsername").value= "$username" 
+$ie.document.getElementById("inputUsername").value = "$username" 
 $ie.document.getElementById("inputPassword").value = "$password" 
 $submit = $ie.document.documentElement.getElementsByClassName("btn btn-primary btn-lg") | Select-Object -First 1
 $submit.click()
@@ -140,19 +159,25 @@ $array = $out.Split(“`n”)
 $script:login = $array[4]
 }
 
+function runbuild {
+oc start-build "$latestbuild"
+}
+
 do
 {
+     cls
      Show-Menu
      $input = Read-Host "Choose from the Main Menu"
      switch ($input)
      {
            '1' {cls; OSlogin}
-           '2' {cls; getprojects} 
+           '2' {cls; getprojects}
            '3' {cls; getpods}
+           '4' {cls; getlatestbuild}
+           '6' {cls; oc edit bc/$latestbuild}
            'c' {cls; customcommand}
-           'r' {cls; rsync} 
-           'l' {cls; oc logout; powershell}
-           'q' {}
+           'r' {cls; rsync}
+           'q' {cls; oc logout}
      }
 }
 until ($input -eq 'q')
